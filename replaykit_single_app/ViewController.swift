f741c788d438c48ae0fa93357b6415f8e700cb6d
batuhansaygin <info@batuhansaygin.com>
2019-03-04 11:57:02 +0300

    Initial Commit

---
+++ replaykit_single_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  replaykit_single_app
+//
+//  Created by Batuhan Saygin on 4.03.2019.
+//  Copyright © 2019 Batuhan Saygin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ replaykit_single_appTests/replaykit_single_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  replaykit_single_appTests.swift
+//  replaykit_single_appTests
+//
+//  Created by Batuhan Saygin on 4.03.2019.
+//  Copyright © 2019 Batuhan Saygin. All rights reserved.
+//
+
+import XCTest
+@testable import replaykit_single_app
+
+class replaykit_single_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
